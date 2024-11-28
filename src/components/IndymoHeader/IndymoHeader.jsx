57a8b6510e059d29707d3e06dee8be21e5b46cb4
Delano <Novasigma89@gmail.com>
2024-11-28 16:33:39 +0100

    Added base header component (not functional)

---
+++ src/components/IndymoHeader/IndymoHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function IndymoHeader(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default IndymoHeader;
